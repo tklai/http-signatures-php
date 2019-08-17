6cd50aa9da16463a33f49d8823a47d5a4ef27bb0
Liam Dennehy <liam@wiemax.net>
2019-08-17 13:23:22 +0200

    KeyStoreTest - Inject additional keys

--- tests/KeyStoreTest.php
+++ tests/KeyStoreTest.php
@@ -7,6 +7,35 @@ use PHPUnit\Framework\TestCase;
 
 class KeyStoreTest extends TestCase
 {
+    public function testKeyStore()
+    {
+        $ks = new KeyStore(['testkey' => 'abc123']);
+        $this->assertEquals(
+          'abc123',
+          $ks->fetch('testkey')->getSigningKey()
+        );
+        $this->assertEquals(
+          'secret',
+          $ks->fetch('testkey')->getType()
+        );
+        $ks = $ks->withKeys([
+          'testkey2' => 'def456',
+          'testkey3' => 'foo-bar',
+        ]);
+        $this->assertEquals(
+          3,
+          $ks->getCount()
+        );
+        $this->assertEquals(
+          'abc123',
+          $ks->fetch('testkey')->getSigningKey()
+        );
+        $this->assertEquals(
+          'secret',
+          $ks->fetch('testkey2')->getType()
+        );
+    }
+
     public function testFetchFail()
     {
         $ks = new KeyStore(['id' => 'secret']);
